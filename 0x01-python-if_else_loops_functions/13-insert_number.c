c7a55853417d5b6dc6b68b196bbbd3f7810aee96
leobyeon <386@holbertonschool.com>
2018-09-11 23:51:43 +0000

    Adding 13

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,67 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * insert_node - inserts a number into a sorted singly linked list
+ * @head: pointer to head of list
+ * @number: number to insert
+ * Return: the head of new list
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *trav;
+	listint_t *trav2;
+	listint_t *tmp;
+
+	trav = *head;
+
+	tmp = malloc(sizeof(listint_t));
+	if (!tmp)
+		return (NULL);
+
+	tmp->n = number;
+	tmp->next = NULL;
+
+	if (!trav)
+	{
+		trav = tmp;
+		return (trav);
+	}
+
+	if (trav->next == NULL)
+	{
+		if (trav->n < number)
+		{
+			trav->next = tmp;
+			return (trav);
+		}
+		else
+		{
+			tmp->next = trav;
+			return (trav);
+		}
+	}
+
+	while (trav)
+	{
+		trav2 = trav->next;
+		if ((trav->n < number) && (trav2->n < number))
+		{
+			trav = trav->next;
+			trav2 = trav2->next;
+		}
+		else if ((trav->n < number) && (trav2->n >= number))
+		{
+			tmp->next = trav2;
+			trav->next = tmp;
+			break;
+		}
+		else if ((trav->n < number) && (trav2->next == NULL))
+		{
+			tmp->next = NULL;
+			trav2->next = tmp;
+			break;
+		}
+	}
+	return (trav);
+}
