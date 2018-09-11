a45f72b5b74d73311251049e6bd3fb5da5efc66b
leobyeon <386@holbertonschool.com>
2018-09-11 20:04:57 +0000

    Did a reworking of the logic: reduced code length by adjusting the range

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(i, 10):
-        if i != j:
-            print("{:d}{:d}, ".format(i, j), end="")
-print("89")
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        print("{:d}{:d}, ".format(i, j), end="")
+print("{:d}".format(89))
