5a294a714a7684b72bbfaced374642bacff78c2c
leobyeon <386@holbertonschool.com>
2018-09-11 21:47:20 +0000

    Adding the correct 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in str:
-        print("{:s}".format(ord(str[i])), end="")
+    for i in range(len(str)):
+        if ord(str[i]) >= 97:
+            print("{:c}".format(ord(str[i]) - 32), end="")
+        else:
+            print("{:s}".format(str[i]), end="")
     print("")
