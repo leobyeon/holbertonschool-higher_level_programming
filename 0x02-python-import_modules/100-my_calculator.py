c7e53d29fa9354887d04c2ab05eb9e47d5719251
leobyeon <386@holbertonschool.com>
2018-09-13 23:00:52 +0000

    Added int for the variables

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,15 +3,15 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
     arg = sys.argv
-    v1 = arg[1]
-    v2 = arg[3]
-    op = arg[2]
     error1 = "Usage: ./100-my_calculator.py <a> <operator> <b>"
     error2 = "Unknown operator. Available operators: +, -, * and /"
     if len(arg) != 4:
         print("{:s}".format(error1))
         exit(1)
     else:
+        v1 = int(arg[1])
+        v2 = int(arg[3])
+        op = arg[2]
         if op == '+':
             print("{:d} + {:d} = {:d}".format(v1, v2, add(v1, v2)))
         elif op == '-':
