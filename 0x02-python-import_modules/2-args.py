d2d381e86a2deb741aa53b957ea425768f0024d5
leobyeon <386@holbertonschool.com>
2018-09-13 21:08:06 +0000

    Adding 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    argv = sys.argv
+    if len(argv) <= 1:
+        print("{:d} arguments.".format(len(argv)-1))
+    elif len(argv) == 2:
+        print("{:d} argument:".format(len(argv)-1))
+        print("{:d}: {:s}".format(len(argv)-1, argv[1]))
+    else:
+        print("{:d} arguments:".format(len(argv)-1))
+        for i in range(1, len(argv)):
+            print("{:d}: {:s}".format(i, argv[i]))
