2964c5c3e7565901b7ab98df99eb2f38cd057f33
leobyeon <386@holbertonschool.com>
2018-09-18 21:19:38 +0000

    Added an if case for longer tuples and shorter tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,8 +2,10 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     x = tuple_a[:]
     y = tuple_b[:]
-    if len(tuple_b) == 0 or len(tuple_b) == 1:
+    if len(tuple_b) == 0:
         y = (0, 0)
+    elif len(tuple_b) == 1:
+        y = (tuple_b[0], 0)
     elif len(tuple_b) < 2:
         y = (tuple_b[0:2])
     return tuple(map(sum, zip(x, y)))
