56425f2895134015cfe646cfe6745173e1db375e
leobyeon <386@holbertonschool.com>
2018-09-18 21:21:48 +0000

    Added a whole new case for tuple_a having different lengths

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,4 +8,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
         y = (tuple_b[0], 0)
     elif len(tuple_b) < 2:
         y = (tuple_b[0:2])
+    if len(tuple_a) == 0:
+        x = (0, 0)
+    elif len(tuple_a) == 1:
+        x = (tuple_a[0], 0)
+    elif len(tuple_a) < 2:
+        x = (tuple_a[0:2])
     return tuple(map(sum, zip(x, y)))
