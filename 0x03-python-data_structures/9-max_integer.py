44572cc9c8f2d477bbf77be1fb6ac2516f844d2d
leobyeon <386@holbertonschool.com>
2018-09-18 21:58:24 +0000

    Changed the if condition for None

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    max = 0
     if my_list is None:
         return None
+    max = my_list[0]
     for i in my_list:
         if i > max:
             max = i
