3cb38a9b44361a2800b4a879d90253d112e97361
leobyeon <386@holbertonschool.com>
2018-09-18 21:30:39 +0000

    Fixed a comparison operator that was incorrectly pointing

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,12 +6,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
         y = (0, 0)
     elif len(tuple_b) == 1:
         y = (tuple_b[0], 0)
-    elif len(tuple_b) < 2:
+    elif len(tuple_b) > 2:
         y = (tuple_b[0:2])
     if len(tuple_a) == 0:
         x = (0, 0)
     elif len(tuple_a) == 1:
         x = (tuple_a[0], 0)
-    elif len(tuple_a) < 2:
+    elif len(tuple_a) > 2:
         x = (tuple_a[0:2])
     return tuple(map(sum, zip(x, y)))
