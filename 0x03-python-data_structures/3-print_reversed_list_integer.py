073d47b1abd3f9e8ded209f24753170948029272
leobyeon <386@holbertonschool.com>
2018-09-19 17:03:34 +0000

    Refactored using list comprehension

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,5 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return None
-    for i in range(len(my_list), 0, -1):
-        print("{:d}".format(my_list[i-1]))
+    my_list.reverse()
+    [print("{:d}".format(x)) for x in my_list]
