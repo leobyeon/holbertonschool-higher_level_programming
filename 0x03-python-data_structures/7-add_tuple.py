97fe521bd2b25b1a40eadc3586eae5816523178b
leobyeon <386@holbertonschool.com>
2018-09-19 01:37:04 +0000

    Refactored to make the code even shorter

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    x = tuple_a + (0, 0)
-    y = tuple_b + (0, 0)
-    x = x[0:2]
-    y = y[0:2]
+    x = (tuple_a + (0, 0))[0:2]
+    y = (tuple_b + (0, 0))[0:2]
     return tuple(map(sum, zip(x, y)))
