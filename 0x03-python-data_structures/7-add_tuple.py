9183d3c9f526416cc24185c2aa2299ad26df20f6
leobyeon <386@holbertonschool.com>
2018-09-19 00:41:14 +0000

    Refactored 7

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    x = tuple_a[:]
-    y = tuple_b[:]
-    if len(tuple_b) == 0:
-        y = (0, 0)
-    elif len(tuple_b) == 1:
-        y = (tuple_b[0], 0)
-    elif len(tuple_b) > 2:
-        y = (tuple_b[0:2])
-    if len(tuple_a) == 0:
-        x = (0, 0)
-    elif len(tuple_a) == 1:
-        x = (tuple_a[0], 0)
-    elif len(tuple_a) > 2:
-        x = (tuple_a[0:2])
+    x = tuple_a + (0, 0)
+    y = tuple_b + (0, 0)
+    x = x[0:2]
+    y = y[0:2]
     return tuple(map(sum, zip(x, y)))
