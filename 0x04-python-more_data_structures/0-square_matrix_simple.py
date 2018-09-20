5041193c67a6cbce0f1d7954f379b839f2dd03d5
leobyeon <386@holbertonschool.com>
2018-09-20 19:17:07 +0000

    Used map, lambda, list, append

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [[i[j]**2 for i in matrix] for j in range(0, len(matrix))]
+    answer = []
+    for i in matrix:
+        answer.append(list(map(lambda x: x**2, i)))
+    return answer
