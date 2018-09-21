c0749b868875121f0c56f3ef98aeaf57875501a4
leobyeon <386@holbertonschool.com>
2018-09-21 00:04:32 +0000

    Shortened variable name

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     d = a_dictionary
-    if a_dictionary is None or len(a_dictionary) is 0:
+    if d is None or len(d) is 0:
         return None
     return sorted([(v, k) for k, v in d.items()], reverse=True)[0][1]
