6061f68211c476faf076c959e35ab27a99ccc454
leobyeon <386@holbertonschool.com>
2018-09-21 16:35:52 +0000

    Refactored using ^

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    mylist = {x for x in set_1 if x not in set_2}
-    mylist.update({x for x in set_2 if x not in set_1})
-    return mylist
+    return set_1 ^ set_2
