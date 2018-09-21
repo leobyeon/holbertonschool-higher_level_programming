476e21cc4bfa494df84b87e694f9030fe347d655
leobyeon <386@holbertonschool.com>
2018-09-21 16:54:04 +0000

    Refactored using dictionary comprehension

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    results = {}
-    for key in a_dictionary:
-        results[key] = a_dictionary[key] * 2
-    return results
+    return {k : v * 2 for k,v in a_dictionary.items()}
