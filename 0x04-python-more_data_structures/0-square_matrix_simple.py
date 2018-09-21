67a7bd28b2dcbef20606cf6a2cfdc6cea37a1a5b
leobyeon <386@holbertonschool.com>
2018-09-21 16:29:16 +0000

    Refactoring using list comprehension

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    answer = []
-    for i in matrix:
-        answer.append(list(map(lambda x: x**2, i)))
-    return answer
+    return [[i * i for i in j] for j in matrix]
