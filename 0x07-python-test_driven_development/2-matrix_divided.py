ec59c62957580fa655b83bafa175d42f66352cbf
leobyeon <386@holbertonschool.com>
2018-10-05 21:48:50 +0000

    Initial push for 0, 1, 2, 3, 4 and README

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def matrix_divided(matrix, div):
+    """function divides a matrix"""
+    rowlen = len(matrix[0])
+    new = []
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
+    elif div is 0:
+        raise ZeroDivisionError("division by zero")
+    for i in matrix:
+        innernew = []
+        if rowlen is not len(i):
+            raise TypeError(
+                    "Each row of the matrix must have the same size")
+        for j in i:
+            if type(j) is not int and type(j) is not float:
+                raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
+            innernew.append(round(j / div, 2))
+        new.append(innernew)
+    return new
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def text_indentation(text):
+    """indents text string by .:?"""
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+    if text is '\n':
+        print()
+        return
+    textlist = text.split(' ')
+    new1 = " ".join(textlist)
+    new2 = ""
+    flag = 1
+    for i in range(len(new1)):
+        if flag and new1[i] is ' ':
+            continue
+        if new1[i] in ".:?":
+            flag = 1
+            new2 += "{}\n\n".format(new1[i])
+        else:
+            flag = 0
+            new2 += new1[i]
+    new2 += '\n'
+    print(new2)
