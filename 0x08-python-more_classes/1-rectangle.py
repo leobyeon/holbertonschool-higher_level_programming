823ce046caf51b9fb79f90fc103317f134bde9b6
leobyeon <386@holbertonschool.com>
2018-10-09 17:04:23 +0000

    Switched the error checkers

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,10 +14,10 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set the width"""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
         self.__width = value
 
     @property
@@ -28,8 +28,8 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set the height"""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
         self.__height = value
