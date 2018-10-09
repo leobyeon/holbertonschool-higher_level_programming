0dae8400ed33504fe20fc11d3aa9867c5014a7cc
leobyeon <386@holbertonschool.com>
2018-10-09 17:54:44 +0000

    Replaced isinstance with type again

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 class Rectangle:
     """define class rectangle"""
+
     def __init__(self, width=0, height=0):
         """initialize"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -14,7 +15,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """set the width"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -28,7 +29,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """set the height"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
