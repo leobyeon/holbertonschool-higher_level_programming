8dd6347bd87eaa75ec457353fc8f49990255a8d2
leobyeon <386@holbertonschool.com>
2018-10-10 18:15:11 +0000

    Refactored 5 using recursion

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,22 +3,11 @@ def text_indentation(text):
     """indents text string by .:?"""
     if type(text) is not str:
         raise TypeError("text must be a string")
-    if text is '\n':
-        print()
-        return
-    if text is "":
-        return
-    textlist = text.split(' ')
-    new1 = " ".join(textlist)
-    new2 = ""
-    flag = 1
-    for i in range(len(new1)):
-        if flag and new1[i] is ' ':
-            continue
-        if new1[i] in ".:?":
-            flag = 1
-            new2 += "{}\n".format(new1[i])
-        else:
-            flag = 0
-            new2 += new1[i]
-    print(new2)
+
+    for i, x in enumerate(text):
+        if x in ".:?":
+            print("{}\n".format(text[:i + 1].lstrip()))
+            text_indentation(text[i + 1:])
+            return
+        print("{}".format(text.lstrip()), end='')
+
