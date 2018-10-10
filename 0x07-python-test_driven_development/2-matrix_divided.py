024cec5ec1f7534b12c7c4a99eae5919498e753a
leobyeon <386@holbertonschool.com>
2018-10-10 17:51:06 +0000

    Fixed 2 using in-conditional for loops

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,14 @@ def matrix_divided(matrix, div):
     """function divides a matrix"""
     rowlen = len(matrix[0])
     new = []
-    if type(div) is not int and type(div) is not float:
+
+    if ((type(matrix) is not list) or
+        (not all(type(row) is list for row in matrix)) or
+        (not all(
+        type(i) in (float, int) for row in matrix for i in row))):
+            raise TypeError("matrix must be a matrix (list of lists) \
+of integers/floats")
+    if type(div) not in (float, int):
         raise TypeError("div must be a number")
     elif div is 0:
         raise ZeroDivisionError("division by zero")
@@ -12,10 +19,6 @@ def matrix_divided(matrix, div):
         if rowlen is not len(i):
             raise TypeError("Each row of the matrix must have the same size")
         for j in i:
-            print(j)
-            if type(j) is not int and type(j) is not float:
-                raise TypeError("matrix must be a matrix (list of lists) \
-of integers/floats")
             innernew.append(round(j / div, 2))
         new.append(innernew)
     return new
