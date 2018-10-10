d2ea2ea6684bb0db0a5de246fae30c2dd2bbfe2a
leobyeon <386@holbertonschool.com>
2018-10-10 21:24:26 +0000

    Refactored using enumerate

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python3
 def matrix_divided(matrix, div):
     """function divides a matrix"""
-    rowlen = len(matrix[0])
-    new = []
-
     if ((type(matrix) is not list) or
         (not all(type(row) is list for row in matrix)) or
             (not all(
             type(i) in (float, int) for row in matrix for i in row))):
                 raise TypeError("matrix must be a matrix (list of lists) \
 of integers/floats")
+    
     if type(div) not in (float, int):
         raise TypeError("div must be a number")
-    elif div is 0:
+    
+    if div == 0:
         raise ZeroDivisionError("division by zero")
-    for i in matrix:
-        innernew = []
-        if rowlen is not len(i):
+
+    for i, ls in enumerate(matrix):
+        if len(ls) != len(matrix[i - 1]):
             raise TypeError("Each row of the matrix must have the same size")
-        for j in i:
-            innernew.append(round(j / div, 2))
-        new.append(innernew)
-    return new
+
+    return [[round(x/div, 2) for x in y] for y in matrix]
