0e95474b02515dd5e0dbacefad966c608828295e
leobyeon <386@holbertonschool.com>
2018-10-17 20:58:10 +0000

    Refactored using for loop

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,20 +3,16 @@ def read_lines(filename="", nb_lines=0):
     """reads n lines of a text file and prints"""
     with open(filename, encoding="utf-8") as myFile:
 
-        linenum = -1
+        linecount = 0
 
-        while True:
-            line = myFile.readline()
-            linenum += 1
-            if not line:
-                break
+        for line in myFile:
+            linecount += 1
+
     myFile.close()
 
     with open(filename, encoding="utf-8") as myFile:
 
-        print("{} : {}".format(nb_lines, linenum))
-
-        if nb_lines <= 0 or nb_lines >= linenum:
+        if nb_lines <= 0 or nb_lines >= linecount:
             print(myFile.read(), end="")
         else:
             while nb_lines != 0:
