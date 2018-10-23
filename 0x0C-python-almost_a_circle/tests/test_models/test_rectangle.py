cfbcb7987f1c7a1af9c0ee670be5038c69b88c75
leobyeon <386@holbertonschool.com>
2018-10-23 05:29:52 +0000

    Forgot to put the word test

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 """test module for rectangle"""
-
-
 import unittest
-import json
 import sys
 from io import StringIO
 from models.base import Base
@@ -13,37 +10,25 @@ from models.rectangle import Rectangle
 class RectangleTests(unittest.TestCase):
     """test cases for Rectangle class"""
 
-    def setup(self):
-        """set up"""
-        self.r1 = Rectangle(1, 1)
-        self.r2 = Rectangle(1, 2, 1, 2)
-        self.capturedOutput = io.StringIO()
-        sys.stdout = self.capturedOutput
-
-    def breakdown(self):
-        """break down"""
-        sys.stdout = sys.__stdout__
-        Base._Base__nb_objects = 0
-
-    def value_error(self):
+    def test_value_error(self):
         """test to check value errors"""
         with self.assertRaises(TypeError):
-            Rectangle(None, 6, 6, 6, 6)
-            Rectangle(6, None, 6, 6, 6)
-            Rectangle(6, 6, None, 6, 6)
-            Rectangle(6, 6, 6, None, 6)
-            Rectangle(6, 6, 6, 6, None)
+            test1 = Rectangle(None, 6, 6, 6, 6)
+            test2 = Rectangle(6, None, 6, 6, 6)
+            test3 = Rectangle(6, 6, None, 6, 6)
+            test4 = Rectangle(6, 6, 6, None, 6)
+            test5 = Rectangle(6, 6, 6, 6, None)
 
-    def type_error(self):
+    def test_type_error(self):
         """test to check type errors"""
         with self.assertRaises(ValueError):
-            Rectangle(0, 6, 6, 6, 6)
-            Rectangle(6, 0, 6, 6, 6)
-            Rectangle(6, 6, 0, 6, 6)
-            Rectangle(6, 6, 6, 0, 6)
-            Rectangle(6, 6, 6, 6, 0)
+            test1 = Rectangle(0, 6, 6, 6, 6)
+            test2 = Rectangle(6, 0, 6, 6, 6)
+            test3 = Rectangle(6, 6, 0, 6, 6)
+            test4 = Rectangle(6, 6, 6, 0, 6)
+            test5 = Rectangle(6, 6, 6, 6, 0)
 
-    def error_messages(self):
+    def test_error_messages(self):
         """test to check correct error messages"""
         with self.assertRaises(TypeError) as e:
             Rectangle("satan's poop", 1)
