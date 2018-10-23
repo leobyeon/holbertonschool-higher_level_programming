83b68954c16c4c8320151c547c2af9c946d82b2c
leobyeon <386@holbertonschool.com>
2018-10-23 03:25:34 +0000

    Adding test_base

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+import unittest
+from models.base import Base
+
+
+class BaseTests(unittest.TestCase):
+    """define class for the tests"""
+
+    def test_1arg(self):
+        b = Base(2)
+        self.assertEqual(2, b.id)
+
+    def test_int(self):
+        """test integer"""
+        c = Base(666)
+        self.assertEqual(666, c.id)
+
+    def test_floats(self):
+        """test floats"""
+        d = Base(6.66)
+        self.assertEqual(6.66, d.id)
+        d = Base(-6.66)
+        self.assertEqual(-6.66, d.id)
+
+    def test_str(self):
+        """test strings"""
+        e = Base("6")
+        self.assertEqual("6", e.id)
+        e = Base("666")
+        self.assertEqual("666", e.id)
+
+    def test_class(self):
+        """test class type"""
+        f = Base()
+        self.assertFalse(isinstance(type(f), Base))
+
+    def test_none(self):
+        """test none and empty"""
+        g = Base(None)
+        self.assertEqual(2, g.id)
+        g = Base()
+        self.assertEqual(3, g.id)
+
+    if __name__ = "__main__":
+        unittest.main()
