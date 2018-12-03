fc5b7a21cd3881fb66dce5f3ee8ad647cc9ce481
leobyeon <386@holbertonschool.com>
2018-12-03 22:47:49 +0000

    Moved name from top to bottom

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in MySQL server
-SELECT score, name
+SELECT score
 FROM second_table
-ORDER BY score DESC;
+ORDER BY score DESC, name;
