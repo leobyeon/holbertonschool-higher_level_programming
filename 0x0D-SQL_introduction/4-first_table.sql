3bfbbf98be44dd95ce5216a0ea651e43a6a4ef8f
leobyeon <386@holbertonschool.com>
2018-12-03 20:46:06 +0000

    Adding 4

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- creates a table called first_table in the current database in MySQL server
+-- first_table:
+-- id INT
+-- name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+	);
