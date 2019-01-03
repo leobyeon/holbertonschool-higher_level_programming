9a3a43e186982e27ea165a4e59466c105bef9f25
leobyeon <386@holbertonschool.com>
2019-01-03 20:28:43 +0000

    pep8

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+
+
 """
 contains the class definition of a State and
 an instance Base = declarative_base()
 """
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class State(Base):
     """ State class definition """
 
     __tablename__ = 'states'
-    
+
     id = Column(
                 Integer,
                 unique=True,
