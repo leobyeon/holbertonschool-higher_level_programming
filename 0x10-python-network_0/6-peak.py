5b001fc046af78717dd2880bcfdae7559625f9b6
leobyeon <386@holbertonschool.com>
2019-01-16 06:55:51 +0000

    Just sorting and getting the largest

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,19 +4,8 @@
 
 def find_peak(list_of_integers):
     arr = list_of_integers
-    lo = 0
-    hi = len(arr) - 1
-    while lo < hi:
-        mid = int((lo + hi) / 2)
-        if arr[mid - 1] <= arr[mid] and arr[mid + 1] <= arr[mid]:
-            return arr[mid]
-        elif arr[mid - 1] > arr[mid]:
-            hi = mid - 1
-        else:
-            lo = mid + 1
-        if mid == lo:
-            if arr[lo] > arr[hi]:
-                return arr[lo]
-            else:
-                return arr[hi]
-    return None
+    if arr != []:
+        arr.sort()
+        return arr[-1]
+    else:
+        return None
