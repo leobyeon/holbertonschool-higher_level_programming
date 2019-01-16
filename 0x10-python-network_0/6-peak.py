87af05be0348ecd8ffc49ea13412b509a408a72d
leobyeon <386@holbertonschool.com>
2019-01-16 06:59:40 +0000

    Adding 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-""" finds a peak in an unsorted list of integers """
 
 
 def find_peak(list_of_integers):
-    arr = list_of_integers
-    if arr != []:
-        arr.sort()
-        return arr[-1]
+""" finds a peak in an unsorted list of integers """
+    if list_of_integers != []:
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
         return None
