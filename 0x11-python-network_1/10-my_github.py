e55e087b1cf1710537d627e41c2c142750b36765
leobyeon <386@holbertonschool.com>
2019-01-22 05:44:20 +0000

    Adding 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# takes your Github credentials (username and password)
+# and uses the Github API to display your id
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    r = requests.get(
+            'https://api.github.com/user', auth=(argv[1], argv[2]))
+    jfile = r.json()
+    print(jfile['id'])
