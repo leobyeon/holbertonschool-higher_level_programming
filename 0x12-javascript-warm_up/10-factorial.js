622dc4876346e2843dbc7cee8d2d0432a4d697a5
leobyeon <386@holbertonschool.com>
2019-02-12 20:49:46 +0000

    Missing a space

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 console.log(factorial(parseInt(process.argv[2])));
-function factorial(n) {
+function factorial (n) {
   if (!n || n === 1) { return 1; } else { return n * factorial(n - 1); }
-};
+}
