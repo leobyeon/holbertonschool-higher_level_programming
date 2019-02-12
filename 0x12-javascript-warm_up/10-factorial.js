98691c21763ce4275d88e2970796dee330b32a90
leobyeon <386@holbertonschool.com>
2019-02-12 20:21:36 +0000

    js semistandard :)

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 console.log(factorial(parseInt(process.argv[2])));
 function factorial(n) {
-  if (!n || n == 1)
-    return 1;
-  else
-    return n * factorial(n - 1);
+  if (!n || n === 1) { return 1; } else { return n * factorial(n - 1); }
 }
