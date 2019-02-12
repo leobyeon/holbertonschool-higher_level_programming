3e62fa63e101ed6ee5f7a0131ac219f8ce63e9e1
leobyeon <386@holbertonschool.com>
2019-02-12 20:27:45 +0000

    semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-if (process.argv.length < 3)
+if (process.argv.length < 4) {
   console.log(0);
-else {
-  let arr = []
+} else {
+  let arr = [];
   for (let i = 2; i < process.argv.length; i++) {
     arr.push(process.argv[i]);
   }
-  console.log(arr.sort()[arr.length-2]);
+  console.log(arr.sort()[arr.length - 2]);
 }
