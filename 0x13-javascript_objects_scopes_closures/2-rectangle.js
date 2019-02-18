3c1a4ae92ee866b5e6effeb524714c29b3e48863
leobyeon <386@holbertonschool.com>
2019-02-18 20:21:37 +0000

    Initial commit

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,8 +1,8 @@
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
 }
