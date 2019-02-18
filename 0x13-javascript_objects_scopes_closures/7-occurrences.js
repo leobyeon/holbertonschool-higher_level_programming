a88a6c0e611b0c8af3918e1ddcd50fe13dccfa62
leobyeon <386@holbertonschool.com>
2019-02-18 21:23:54 +0000

    Refactored 7 using a count variable instead of a frequency counter

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-  let freqMap = {};
-  list.forEach(function (x) { (freqMap[x]) ? freqMap[x] += 1 : freqMap[x] = 1; });
-  return freqMap[searchElement];
+  let count = 0;
+  list.forEach(function (x) { if (x === searchElement) { count++; } });
+  return count;
 };
