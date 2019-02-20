8a1c7872917d830fe9032641206570fa9604eb6c
leobyeon <386@holbertonschool.com>
2019-02-20 22:26:30 +0000

    Fixed typo

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,7 @@ const request = require('request');
 request(process.argv[2], (err, resp, body) => {
   if (err) console.log(err);
   else {
-    fs.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
+    fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
       if (err) console.log(err);
     });
   }
